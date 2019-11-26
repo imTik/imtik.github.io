37c1eb5d4790ba3ffe2d5a0c3f26116146d31ce4
Tik Guan <275151754@qq.com>
2019-11-26 11:08:37 +0800

    update

--- src/utils/components.js
+++
@@ -1,9 +0,0 @@
-import globalLoading from '../components/globalLoading'
-
-const allComponents = {
-  install: function(Vue) {
-    Vue.component('globalLoading', globalLoading);
-  }
-};
-
-export default allComponents
